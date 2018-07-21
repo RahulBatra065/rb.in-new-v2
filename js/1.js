a5e0b593f7c850c1a821b46e9efafe44453dd754
Rahul Batra <rahulbatra065@outlook.com>
2018-07-21 09:46:53 +0530

    cards

--- js/1.js
+++ js/1.js
@@ -1,5 +1,6 @@
-var img = ["1.jpg", "2.jpg", "3.png", "4.png"]
+var img = ["1.jpg", "2.jpg", "3.png", "4.png","5.png"]
      function imgRandom(imgArr) {
         return imgArr[Math.floor(Math.random() * imgArr.length)];
     }
 document.getElementById('image').src = imgRandom(img);
+console.log("Love peeking under the hood?, I have just the job for you! Click here to know more!")
