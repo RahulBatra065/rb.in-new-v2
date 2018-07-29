9a6c2a20ae4c391d116f8b8e52d14e793c0a5810
Rahul Batra <rahulbatra065@outlook.com>
2018-07-29 15:03:14 +0530

    menu

--- js/1.js
+++ js/1.js
@@ -3,4 +3,22 @@ var img = ["1.jpg", "2.jpg", "3.png", "4.png","5.png"]
         return imgArr[Math.floor(Math.random() * imgArr.length)];
     }
 document.getElementById('image').src = imgRandom(img);
-console.log("Love peeking under the hood?, I have just the job for you! Click here to know more!")
+console.log("Love peeking under the hood?, I have just the job for you! Click here to know more!");
+var menu = $('.wrapper div');
+// menu magic
+$('.nav-toggle').click(function() {
+	$('.line').fadeOut("fast");
+$('.sidemenu').removeClass('side');
+$('.sidemen').removeClass('side');
+
+});
+// close the menu
+$('.cross').click(function() {
+	$('.line').fadeIn("fast");
+$('.sidemenu').addClass('side');
+$('.sidemen').addClass('side');
+
+});
+//auto disappear of menu onLoad Document
+$('.sidemenu').addClass('side');
+$('.sidemen').addClass('side');
