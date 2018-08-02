512ca227af33397e6e8fb361c1a7eb48a2f60f49
Rahul Batra <rahulbatra065@outlook.com>
2018-08-02 21:05:51 +0530

    yb

--- js/1.js
+++ js/1.js
@@ -1,38 +1,34 @@
 var img = ["1.jpg", "2.jpg", "3.png", "4.png", "5.png"];
+
 function imgRandom(imgArr) {
-	return imgArr[Math.floor(Math.random() * imgArr.length)];
+    return imgArr[Math.floor(Math.random() * imgArr.length)];
 }
 document.getElementById("image").src = imgRandom(img);
-console.log(
-	"Love peeking under the hood?, I have just the job for you! Click here to know more!"
-);
-
-
-
+console.log("Love peeking under the hood?, I have just the job for you! Click here to know more!");
 $('.one').click(function() {
-window.location.href = '/work/yearbook';
+    window.location.href = '/work/yearbook';
 });
 $('.two').click(function() {
-window.location.href = '/work/alphanode';
+    window.location.href = '/work/alphanode';
 });
 $('.three').click(function() {
-window.location.href = '/work/iumun';
+    window.location.href = '/work/iumun';
 });
 $('.four').click(function() {
-window.location.href = '/work/umun';
-});
-$('.four').click(function() {
-window.location.href = '/work/ecomm';
+    window.location.href = '/work/umun';
 });
 $('.five').click(function() {
-window.location.href = '/work/farewell';
+    window.location.href = '/work/ecomm';
+});
+$('.six').click(function() {
+    window.location.href = '/work/farewell';
 });
 $('.fa-instagram').click(function() {
-window.location.href = 'https://www.instagram.com/rahulbatra65/';
+    window.location.href = 'https://www.instagram.com/rahulbatra65/';
 });
 $('.fa-facebook').click(function() {
-window.location.href = 'https://www.facebook.com/rahul.batra.581730';
+    window.location.href = 'https://www.facebook.com/rahul.batra.581730';
 });
 $('.fa-medium').click(function() {
-window.location.href = 'https://medium.com/@Retr0Br0';
+    window.location.href = 'https://medium.com/@Retr0Br0';
 });
--- js/menu.js
+++ js/menu.js
@@ -1,20 +1,19 @@
 // menu magic by RB
 $(".nav-toggle").click(function() {
-	$(".line").fadeOut("fast");
-	$(".sidemenu").removeClass("side");
-	$(".sidemen").removeClass("side");
-	$(".body").addClass("freeze");
+    $(".line").fadeOut("fast");
+    $(".sidemenu").removeClass("side");
+    $(".sidemen").removeClass("side");
+    $(".body").addClass("freeze");
 });
 // close the menu
 $(".cross").click(function() {
-	$(".body").removeClass("freeze");
-	$(".line").fadeIn("fast");
-	$(".sidemenu").addClass("side");
-	$(".sidemen").addClass("side");
+    $(".body").removeClass("freeze");
+    $(".line").fadeIn("fast");
+    $(".sidemenu").addClass("side");
+    $(".sidemen").addClass("side");
 });
-
-   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
         e.preventDefault();
         //basically listens to all anchors being clicked on
         document.querySelector(this.getAttribute('href')).scrollIntoView({
